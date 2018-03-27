ff23b43a9f3c2d1d8d525fff7424a3f1c8a9b05b
Pan Li <panli@microsoft.com>
2018-03-27 14:48:25 +0800

    Add Annotations for graph
    * One graph consist of edges and vertexes
    
    * @Graph specify the domain class as one graph
    
    * @EdgeSet specify one field in graph for all edges
    
    * @VertexSet specify one field in graph for all vertexes
    
    Signed-off-by: Pan Li <panli@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/annotation/EdgeSet.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.annotation;
+
+import org.springframework.data.annotation.Persistent;
+
+import java.lang.annotation.*;
+
+/**
+ * Specifies the field as EdgeSet of graph.
+ * @author Incarnation-p-lee
+ */
+@Persistent
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface EdgeSet {
+}
+
